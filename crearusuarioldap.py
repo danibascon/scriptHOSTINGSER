91e8020dff2a6b62c6a187226de7e59ec01b58b8
dani <bascon1991@hotmail.es>
2017-12-20 17:36:02 +0100

    cambio

--- crearusuarioldap.py
+++ crearusuarioldap.py
@@ -18,7 +18,7 @@ num.append(int(commands.getoutput("ldapsearch -x | grep '^uidNumber:' | egrep -o
 contra= getpass.getpass("Dime el passwd: ")
 UID='4000'
 if UID in num:
-	UID = int(commands.getoutput("ldapsearch -x | grep '^uidNumber:' | egrep -o [0-9]{4} | tail -n1")) +1
+	UID = int(commands.getoutput("ldapsearch -x | grep '^uidNumber:' | egrep -o [0-9]{4} | sort | tail -n1")) +1
 
 usuario=["dn: uid="+uid+",dc=superhosting,dc=com \n",
 		"objectClass: top \n",
