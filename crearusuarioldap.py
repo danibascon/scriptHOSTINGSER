312836b12d5001b8a4961de0c885b776738d06d7
dani <bascon1991@hotmail.es>
2017-12-20 09:58:33 +0100

    cambios

--- crearusuarioldap.py
+++ crearusuarioldap.py
@@ -12,18 +12,19 @@ SN=commands.getoutput("echo "+raw_input('Dime el primer apellido: ')+" | base64"
 correo=raw_input("Correo: ")
 uid=""
 lista=[]
-lista = commands.getoutput("slapsearch -x | grep '^uid:'")
+lista = commands.getoutput("ldapsearch -x -h 172.22.200.109 -b 'ou=People,dc=superhosting,dc=com' | grep '^uid:'")
 while uid=="" or uid in lista:
 	uid= raw_input("Dime un uid unico: ") 
-num=int(commands.getoutput("slapsearch -x | grep '^uidNumber:' | egrep -o [0-9]{4}"))
-contra= commands.getoutput("slappasswd -s "+getpass.getpass("Dime el passwd: "))
+num=[]
+num.append(int(commands.getoutput("ldapsearch -x -h 172.22.200.109 -b 'ou=People,dc=superhosting,dc=com' | grep '^uidNumber:' | egrep -o [0-9]{4}")))
+contra= getpass.getpass("Dime el passwd: ")
 UID='4000'
 if UID in num:
-	UID = int(commands.getoutput("slapsearch -x | grep '^uidNumber:' | egrep -o [0-9]{4} | tail -n1")) +1
+	UID = int(commands.getoutput("ldapsearch -x -h 172.22.200.109 -b 'ou=People,dc=superhosting,dc=com'| grep '^uidNumber:' | egrep -o [0-9]{4} | tail -n1")) +1
 
-usuario=["dn: uid='"+uid+",ou=People,dc=bascon,dc=gonzalonazareno,dc=org \n",
+usuario=["dn: uid="+uid+",dc=superhosting,dc=com \n",
 		"objectClass: top \n",
-		"objectClass: posixAccount' \n",
+		"objectClass: posixAccount \n",
 		"objectClass: inetOrgPerson \n",
 		"objectClass: person \n",
 		"cn:: "+CN+" \n",
@@ -37,21 +38,13 @@ usuario=["dn: uid='"+uid+",ou=People,dc=bascon,dc=gonzalonazareno,dc=org \n",
 		"mail: "+correo+" \n",
 		"givenName: "+SN+" \n"]
 
-f=open('usuario.ldif','a')
+f=open('usuario.ldif','w')
 f.writelines(usuario)
 f.close()
 
 
+commands.getoutput("ldapadd -x -h 172.22.200.109 -D 'cn=admin,dc=superhosting,dc=com' -w 'admin' -f usuario.ldif ")
 
 
-
-commands.getoutput("rm -r uid a b cn sn ssha")
-
-
-print "Enter LDAP Password:"
-commands.getoutput("ldapadd -x -D 'cn=admin,dc=bascon,dc=gonzalonazareno,dc=org' -W -f genuser.ldif ")
-
-
-commands.getoutput("sed -i'.bak' '$d' /etc/apache2/sites-available/www.conf")
 
 
