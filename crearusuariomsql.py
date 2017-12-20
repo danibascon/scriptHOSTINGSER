ae8533dcdb9a23a76c779caf88f4b7f66a77d677
dani <bascon1991@hotmail.es>
2017-12-20 10:54:10 +0100

    cambios

--- crearusuariomsql.py
+++ crearusuariomsql.py
@@ -26,19 +26,19 @@ contra=raw_input('Dime la contra: ')
 
 
 
-#mariadb -u root -p 'root' -e 'grant all privileges on bascon.* to 'dani'@'%' identified by 'dani''
+#				mariadb -u root -p 'root' -e 'grant all privileges on bascon.* to 'dani'@'%' identified by 'dani''
 
 
 
 
 
 commands.getoutput("mariadb -h 10.0.0.16 -u admin -p'admin' -e 'create database "+basedatos+"'")
-commands.getoutput("mariadb -h 10.0.0.16 -u admin -p'admin' -e 'grant all privileges on "+basedatos+".* to '"+nombre+"'@'%' identified by '"+contra+"'")
+commands.getoutput("mariadb -h 10.0.0.16 -u admin -p'admin' -e 'grant all privileges on "+basedatos+".* to "'""+nombre+""'"@"'"%"'" identified by "'""+contra+""'"")
 
 
 
 
-
+#		mariadb -h 10.0.0.16 -u admin -p 'admin' -e 'drop database dbascondatabase'
 
 
 
