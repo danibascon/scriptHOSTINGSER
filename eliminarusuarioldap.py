9477aea88860f1b8aea1b1e628daf57e33c14443
dani <bascon1991@hotmail.es>
2017-12-20 17:41:48 +0100

    cambio

--- eliminarusuarioldap.py
+++ eliminarusuarioldap.py
@@ -5,4 +5,4 @@ import commands
 
 usuario=raw_input("Usuario que quieres borrar: ")
 
-commands.getoutput("ldapdelete -x -h 172.22.200.109 -D 'cn=admin,dc=superhosting,dc=com' -w 'admin' -r 'uid="+usuario+",ou=People,dc=com' ")
+commands.getoutput("ldapdelete -x -h 172.22.200.109 -D 'cn=admin,dc=superhosting,dc=com' -w 'admin' -r 'uid="+usuario+",ou=People,dc=superhosting,dc=com' ")
