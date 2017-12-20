2d5aceed3796c0b3a90e707324a4c5e68f706811
dani <bascon1991@hotmail.es>
2017-12-20 10:02:32 +0100

    cambios

--- crearusuarioldap.py
+++ crearusuarioldap.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 # -+- coding: utf-8 .*.
 
-import os
 import commands
-import sys
 import getpass
 
 
@@ -12,15 +10,15 @@ SN=commands.getoutput("echo "+raw_input('Dime el primer apellido: ')+" | base64"
 correo=raw_input("Correo: ")
 uid=""
 lista=[]
-lista = commands.getoutput("ldapsearch -x -h 172.22.200.109 -b 'ou=People,dc=superhosting,dc=com' | grep '^uid:'")
+lista = commands.getoutput("ldapsearch -x  | grep '^uid:'")
 while uid=="" or uid in lista:
 	uid= raw_input("Dime un uid unico: ") 
 num=[]
-num.append(int(commands.getoutput("ldapsearch -x -h 172.22.200.109 -b 'ou=People,dc=superhosting,dc=com' | grep '^uidNumber:' | egrep -o [0-9]{4}")))
+num.append(int(commands.getoutput("ldapsearch -x | grep '^uidNumber:' | egrep -o [0-9]{4}")))
 contra= getpass.getpass("Dime el passwd: ")
 UID='4000'
 if UID in num:
-	UID = int(commands.getoutput("ldapsearch -x -h 172.22.200.109 -b 'ou=People,dc=superhosting,dc=com'| grep '^uidNumber:' | egrep -o [0-9]{4} | tail -n1")) +1
+	UID = int(commands.getoutput("ldapsearch -x | grep '^uidNumber:' | egrep -o [0-9]{4} | tail -n1")) +1
 
 usuario=["dn: uid="+uid+",dc=superhosting,dc=com \n",
 		"objectClass: top \n",
