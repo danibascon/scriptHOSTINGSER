84b05c94e5ae78ce6c72bc44d2104791dd808cce
dani <bascon1991@hotmail.es>
2017-12-20 10:44:51 +0100

    cambios

--- crearusuariomsql.py
+++ crearusuariomsql.py
@@ -33,7 +33,7 @@ contra=raw_input('Dime la contra: ')
 
 
 commands.getoutput("mariadb -h 10.0.0.16 -u admin -p'admin' -e 'create database "+basedatos+"'")
-commands.getoutput("mariadb -h 10.0.0.16 -u admin -p'admin' -e 'grant all privileges on "basedatos".* to '"+nombre+"'@'%' identified by '"+contra+"'")
+commands.getoutput("mariadb -h 10.0.0.16 -u admin -p'admin' -e 'grant all privileges on "+basedatos+".* to '"+nombre+"'@'%' identified by '"+contra+"'")
 
 
 
